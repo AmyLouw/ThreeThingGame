1a3e16d311984174110cdbf517e699bdea6c99f5
amy louw <71879442+AmyLouw@users.noreply.github.com>
2025-03-27 13:17:51 +0000

    placeholder main menu added

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void PressPlay()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void PressQuit()
+    {
+        Application.Quit();
+    }
+}
