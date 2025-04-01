3316f47d6089050b92d284a85ee087f00eb0f665
amy louw <71879442+AmyLouw@users.noreply.github.com>
2025-04-01 12:12:56 +0100

    added corner transform

--- Assets/Scripts/CameraTrigger.cs
+++ Assets/Scripts/CameraTrigger.cs
@@ -14,14 +14,14 @@ public class CameraTrigger : MonoBehaviour
         camera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            // Set the flag to indicate the player is at the corner
-            camera.GetComponent<CameraController>().isAtCorner = true;
-            // Start the coroutine to smoothly transition the camera
-            StartCoroutine(camera.GetComponent<CameraController>().SmoothCameraTransition());
-        }
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        // Set the flag to indicate the player is at the corner
+    //        camera.GetComponent<CameraController>().isAtCorner = true;
+    //        // Start the coroutine to smoothly transition the camera
+    //        StartCoroutine(camera.GetComponent<CameraController>().SmoothCameraTransition());
+    //    }
+    //}
 }
